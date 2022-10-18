4fdcec4e9c4fb2bd2d8b55508445e2b441fae037
stivennevits <stivennevits16@gmail.com>
2022-10-18 11:27:29 -0500

    Sincronizando repositorio

---
+++ RETRO.md
@@ -0,0 +1,18 @@
+## Restropectiva - Spring 1: 
+
+#### ***Start Doing:***
+* Probar comunicación via Wp para mejorar la comunicación del equipo.
+
+#### ***Stop Doing:***
+* Dejar todo a último momento 
+
+#### ***More of:*** 
+* Mayor compromiso.
+
+#### ***keep Doing:***
+* Entregas a tiempo.
+
+#### ***Less Of:***
+* Procrastinar.
+
+
---
+++ app.js
@@ -0,0 +1,41 @@
+
+const EXPRESS = require ("express"); 
+const PATH = require ("path"); 
+const APP = EXPRESS();
+const PUBLIC_PATH = PATH.join(__dirname, "./public");
+const PORT = process.env.PORT || 3000;
+APP.use(EXPRESS.static(PUBLIC_PATH));
+
+APP.listen(PORT, () =>{console.log("Corriendo servidor en el puerto" + " " + PORT + " " + "http://localhost:" + PORT) } )
+
+APP.get("/", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/home.html"))
+});
+
+APP.get("/register", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/register.html"))
+});
+
+APP.get("/login", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/login.html"))
+});
+
+APP.get("/detalleDelProducto", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/detalleDelProducto.html"))
+});
+
+APP.get("/detalle2", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/detalle2.html"))
+});
+
+APP.get("/detalle3", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/detalle3.html"))
+});
+
+APP.get("/detalle4", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/detalle4.html"))
+});
+APP.get("/recuperar-cuenta", (req, res) => {
+    res.sendFile(PATH.resolve(__dirname, "./views/account-recover.html"))
+});
+
